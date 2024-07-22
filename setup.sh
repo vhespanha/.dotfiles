20212d4e64573efa7d2ec99ffcfecb22420b8a62
vinicius <vhespanha@tuta.io>
2024-07-22 14:26:01 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,33 +1,36 @@
-#!/bin/bash
-
-current_dir="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
-dotfiles_source="${current_dir}/home_files"
-
-while read -r file; do
-
-    relative_file_path="${file#"${dotfiles_source}"/}"
-    target_file="${HOME}/${relative_file_path}"
-    target_dir="${target_file%/*}"
-
-    if test ! -d "${target_dir}"; then
-        mkdir -p "${target_dir}"
-    fi
-
-    printf 'Installing dotfiles symlink %s\n' "${target_file}"
-    ln -sf "${file}" "${target_file}"
-
-done < <(find "${dotfiles_source}" -type f)
-
-TMPDIR=$(mktemp -d)
-
-CURRENT=$PWD
-
-cd $TMPDIR
-
-for script in ~/.dotfiles/scripts/*; do
-  bash "$script"
-done
-
-cd $CURRENT
-
-rm -rf $TMPDIR
+  #!/usr/bin/env bash
+  
+  function install_software() {
+  sleep 20
+  sudo apt -o DPkg::Lock::Timeout=600 install build-essential python3-venv socat ncat ruby-dev jq thefuck tmux libfuse2 fuse software-properties-common most -y
+  sudo apt remove bat ripgrep -y
+  sudo apt-get install -y ca-certificates curl gnupg stow neovim luajit go
+  curl -sS https://starship.rs/install.sh | sudo sh -s -- -y
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | sudo sh -s -- -y
+  sudo apt-get update
+  export PATH="$PATH:$HOME/.local/bin"
+  cargo install ripgrep
+  cargo install fd-find
+  cargo install bat --locked
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install --all
+  
+  }
+  
+  function link_files() {
+  mkdir -p ~/.config
+  rm ~/.gitconfig
+  rm -rf ~/.config/zshrc
+  rm -rf ~/.prettierrc
+  rm -rf ~/.editorconfig
+  rm -rf ~/.config/nvim
+  stow .
+  }
+  
+  function setup_software() {
+  git clone git@github.com:vhespanha/nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+  nvim --headless "+Lazy! sync" +qa
+  sudo chsh -s /usr/bin/zsh
+  }
+  
+  echo '🔗 Done!' 
